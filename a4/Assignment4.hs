ab16301b2b303279eaac9580241ed94fa74467dd
Cameron Moy <camoy@cs.umd.edu>
2019-04-15 16:26:32 -0400

    Add a4

---
+++ a4/Assignment4.hs
@@ -0,0 +1,32 @@
+{-
+   Honor Pledge
+
+   I pledge on my honor that I have not given or received any
+   unauthorized assistance on this assignment.
+
+   [Your Name Here]
+-}
+
+module Assignment4 where
+import Data.Void
+
+--
+-- Section 1: Deviations Lazily
+--
+
+trace :: (a -> b -> (c, b)) -> a -> c
+trace f i = o
+  where (o, z) = f i z
+
+deviations :: Fractional a => [a] -> [a]
+deviations = trace deviations'
+
+deviations' :: a -> b -> (c, d)
+deviations' _ _ = error "define me!"
+
+--
+-- Section 2: Encoding Negation in Types
+--
+
+modusTollens :: ((a -> b), (b -> Void)) -> (a -> Void)
+modusTollens _ _ = error "define me!"
